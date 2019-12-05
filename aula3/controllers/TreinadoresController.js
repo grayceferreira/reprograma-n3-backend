1d24169d756ced208446d451fa36ba12e51679cd
Grayce Ferreira <grayce.ferreira@gmail.com>
2019-12-04 22:21:44 -0300

    Autenticação das rotas - Parte 1

--- aula3/controllers/TreinadoresController.js
+++ aula3/controllers/TreinadoresController.js
@@ -4,7 +4,6 @@ const { pokemonsModel } = require('../models/PokemonsSchema')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const CHAVE_PUBLICA = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCOl54HaBM/WiL/jPPdFGjm9f8VprUst1J+vs7G/YRGRHYLGqt+M/ljAhcROPy3FdaVi2smqqyZhf4d+EZ9lKM6LVed91sxvcyMFEp6x8R2KS9wIzUtJ6r1MAIKd8HURmbaN4V2TV/FLeOUANRCZ+QhYEy+eNbuVIJANYtXBUSn8QIDAQAB'
 const CHAVE_PRIVADA = 'MIICXAIBAAKBgQCOl54HaBM/WiL/jPPdFGjm9f8VprUst1J+vs7G/YRGRHYLGqt+M/ljAhcROPy3FdaVi2smqqyZhf4d+EZ9lKM6LVed91sxvcyMFEp6x8R2KS9wIzUtJ6r1MAIKd8HURmbaN4V2TV/FLeOUANRCZ+QhYEy+eNbuVIJANYtXBUSn8QIDAQABAoGBAIuVS/MAJGdNuxjiSA5Q3mfIw03UhWIiirTb39rXbNbESbGRB/NguW38K8yGNoya6hY2BkwxowgeLKX11js0d5sSHgEgL+pDQtXshHu7vlYU0ksHwfmD/R8+ZHJH6F6L0vuzs4NoVK/8iQHFLboUjF2sORyuLHbBmFZQWhInet8pAkEA0OlL2uHCYhkNuokJ9H+OnJEqKS2BtYSkH3Hrh2opZg2HtvUtXEIxzmj/95CzxMXQtNJhQMK3ekvnF3Upcj2avwJBAK67i8OEKM2jerbFKrBqr6/kUkZeyHLA8I4L2C3/3nKPGUj/GAc2xxuK1XxnpC0e3Wqz5OMwzkWU4Ynblsdq2U8CQHu9U6LICbzVHh6YwP7C9xOhoBlXzPZZJGVDssA4j2DVLsednUqCIsIhy0s1uGUazi3sVpJnQwn7H1vzl6ME/j0CQAT7qj+4LCW5LM27j70aPcppW4NQPq0vHW0fn1moe2KO/CydwcSq5kC909rJZeA3ih755GQqRyeq2EfDMGidfncCQD770Za6sJP1/i1vcdoWuWYnhpiU8TNKjFb2vJEN598amcyJV9PlAAdEkszh6EDA76t6/yT6NoUn/y9x4YskzQo='
 connect()
 
@@ -129,6 +128,28 @@ const addPokemon = async (request, response) => {
 }
 
 const treinarPokemon = async (request, response) => {
+  const authHeaderTreinar = request.get('authorization')
+  let autenticado = false
+
+  if (!authHeaderTreinar) {
+    return response.status(401).send('Você precisa fazer login!')
+  }
+
+  const token = authHeaderTreinar.split(' ')[1]
+
+  jwt.verify(token, CHAVE_PRIVADA, (error, decoded) => {
+    if (error) {
+      autenticado = false
+    } else {
+      autenticado = true
+    }
+  })
+
+  if (!autenticado) {
+    return response.status(403).send('Acesso negado.')
+  }
+
+
   const pokemonId = request.params.pokemonId
   const treinadorId = request.params.treinadorId
   const treinador = await treinadoresModel.findById(treinadorId)
@@ -182,6 +203,28 @@ const getPokemons = async (request, response) => {
 }
 
 const updatePokemon = async (request, response) => {
+  const authHeaderUpdate = request.get('authorization')
+  let autenticado = false
+
+  if (!authHeaderUpdate) {
+    return response.status(401).send('Você precisa fazer login!')
+  }
+
+  const token = authHeaderUpdate.split(' ')[1]
+
+  jwt.verify(token, CHAVE_PRIVADA, (error, decoded) => {
+    if (error) {
+      autenticado = false
+    } else {
+      autenticado = true
+    }
+  })
+
+  if (!autenticado) {
+    return response.status(403).send('Acesso negado.')
+  }
+
+
   const treinadorId = request.params.treinadorId
   const pokemonId = request.params.pokemonId
   const options = { new: true }
@@ -206,6 +249,27 @@ const updatePokemon = async (request, response) => {
 }
 
 const getPokemonById = async (request, response) => {
+  const authHeaderPokById = request.get('authorization')
+  let autenticado = false
+
+  if (!authHeaderPokById) {
+    return response.status(401).send('Você precisa fazer login!')
+  }
+
+  const token = authHeaderPokById.split(' ')[1]
+
+  jwt.verify(token, CHAVE_PRIVADA, (error, decoded) => {
+    if (error) {
+      autenticado = false
+    } else {
+      autenticado = true
+    }
+  })
+
+  if (!autenticado) {
+    return response.status(403).send('Acesso negado.')
+  }
+
   const treinadorId = request.params.treinadorId
   const pokemonId = request.params.pokemonId
   const treinador = await treinadoresModel.findById(treinadorId)
--- aula3/routes/treinadores.js
+++ aula3/routes/treinadores.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 const controller = require("../controllers/TreinadoresController")
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3324dce62e2af3acfee3f93829e5f8e4fea3acaa
 router.get('', controller.getAll)
 router.post('', controller.add)
 router.get('/:id', controller.getById)
@@ -14,14 +10,9 @@ router.patch('/:id', controller.update)
 router.delete('/:id', controller.remove)
 router.post('/:treinadorId/pokemons', controller.addPokemon)
 router.patch('/:treinadorId/pokemons/:pokemonId/treinar', controller.treinarPokemon)
-<<<<<<< HEAD
-router.get('/:treinadorId/pokemons/:pokemonId', controller.getPokemonById)
-router.patch('/:treinadorId/pokemons/:pokemonId', controller.updatePokemon)
-=======
 router.get('/:id/pokemons', controller.getPokemons)
 router.patch('/:treinadorId/pokemons/:pokemonId', controller.updatePokemon)
 router.get('/:treinadorId/pokemons/:pokemonId', controller.getPokemonById)
->>>>>>> 3324dce62e2af3acfee3f93829e5f8e4fea3acaa
 router.post('/login', controller.login)
 
 module.exports = router
